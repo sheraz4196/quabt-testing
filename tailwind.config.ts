0ea28575e37de6ed363507112182543883287ea8
sheraz4196 <sheraztoor321@gmail.com>
2024-07-12 16:12:02 +0500

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#6D28D9",
+      },
+      animation: {
+        marquee: "marquee 30s linear infinite",
+        marquee2: "marquee2 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
